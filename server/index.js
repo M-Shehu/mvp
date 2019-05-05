b7316705b7bed786be80d823371509d4661f0163
M-Shehu <muhammadshehu@Shehus-MacBook-Pro.local>
2019-05-04 21:05:34 -0700

    Add routers and renders for SSR

--- server/index.js
+++ server/index.js
@@ -1,16 +1,26 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-const graphqlHTTP = require('express-graphql');
-const morgan = require('morgan');
-var { buildSchema } = require('graphql');
+import renderer from './renderer.js';
+import express from 'express';
+import cors from 'cors';
+import path from  'path';
+import graphqlHTTP from  'express-graphql';
+import morgan from 'morgan';
+import { buildSchema } from 'graphql';
 
 const app = express();
 const port = process.env.PORT || 3456;
 
 app.use(cors());
 app.use(morgan('tiny'));
-app.use(express.static(path.join(__dirname, '/../public/dist')));
+app.use(express.static('public/dist'));
+
+app.get('*', (req, res) => {
+  const result = renderer(req);
+  const context = result.context;
+  if(context && context.status !== undefined) {
+    res.status(context.status);
+  }
+  res.send(result.jsx);
+});
 
 app.listen(port, () => {
   console.log(`Server is now listening on port: ${port}`);
