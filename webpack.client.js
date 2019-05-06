316545740685aeff78178ddb22a97a7b7236926b
M-Shehu <muhammadshehu@Shehus-MacBook-Pro.local>
2019-05-06 15:38:40 -0700

    Update webpack

--- webpack.client.js
+++ webpack.client.js
@@ -3,11 +3,51 @@ const path = require('path');
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base.js');
 
+const SRC_DIR = path.join(__dirname, '/client/src');
+const DIST_DIR = path.join(__dirname, '/public/dist');
+const SVR_DIR = path.join(__dirname, '/server/index.js');
 const Dotenv = require('dotenv-webpack');
 
 const config = {
   context: __dirname,
   entry: ['./client/src/index.jsx'],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: SRC_DIR,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|otf|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/'
+          }
+        }]
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }
+        ]
+      },
+    ]
+  },
   output: {
     path: path.join(__dirname, 'public/dist'),
     filename: 'spriseBundle.js',
@@ -22,41 +62,6 @@ const config = {
   },
   plugins: [
     new Dotenv()
-  ],
-  rules: [
-    {
-      test: /\.(js|jsx)$/,
-      include: SRC_DIR,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-      },
-    },
-    {
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader'],
-    },
-    {
-      test: /\.(woff(2)?|ttf|eot|otf|svg)(\?v=\d+\.\d+\.\d+)?$/,
-      use: [{
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]',
-          outputPath: 'fonts/'
-        }
-      }]
-    },
-    {
-      test: /\.(png|jpg|gif)$/i,
-      use: [
-        {
-          loader: 'url-loader',
-          options: {
-            limit: 8192
-          }
-        }
-      ]
-    },
   ],
 };
 
