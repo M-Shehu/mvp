dfccf93ea0b42b093fff22696213b779e9cea908
M-Shehu <muhammadshehu@Shehus-MacBook-Pro.local>
2019-05-06 10:57:17 -0700

    Cleaned up files

--- client/src/store/createStore.js
+++
@@ -1,8 +0,0 @@
-import { applyMiddleware, createStore } from 'redux';
-import thunk from 'redux-thunk';
-
-import rootReducer from '../reducers/rootReducer';
-
-const middleware = applyMiddleware(thunk);
-
-export default initialState => createStore(rootReducer, initialState, middleware);
