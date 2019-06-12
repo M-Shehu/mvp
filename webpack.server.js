d3b150fbb6895ca587483591a3654ddb4eaa4c71
M-Shehu <magnanimus.shehu@gmail.com>
2019-06-11 22:24:14 -0500

    Update directory routes of server webpack config

--- webpack.server.js
+++ webpack.server.js
@@ -2,6 +2,10 @@ const path = require('path');
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base.js');
 
+const SRC_DIR = path.join(__dirname, '/client/src');
+const DIST_DIR = path.join(__dirname, '/public/dist');
+const SVR_DIR = path.join(__dirname, '/server/index.js');
+
 const config = {
   target: 'node',
   entry: ['./server/index.js'],
@@ -19,6 +23,39 @@ const config = {
           search: 'require(\'firebase/app\');',
           replace: 'require(\'firebase/app\').default;',
         },
+      },
+      {
+        test: /\.(js|jsx)$/,
+        include: SRC_DIR,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['null-loader', 'null-loader'],
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|otf|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [{
+          loader: 'null-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/'
+          }
+        }]
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'null-loader',
+            options: {
+              limit: 8192
+            }
+          }
+        ]
       },
     ],
   },
