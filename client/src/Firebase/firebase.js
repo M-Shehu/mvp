eba27fa17573abb30531f900085ffba608830b4d
M-Shehu <magnanimus.shehu@gmail.com>
2019-08-23 11:18:23 +0100

    Update to get config from config file

--- client/src/Firebase/firebase.js
+++ client/src/Firebase/firebase.js
@@ -1,17 +1,7 @@
 import app from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
-
-const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID
-};
-
+import firebaseConfig from './firebase.config';
 
 class Firebase {
   constructor() {
